e88f3acdbe2186bf8cb60866f08f9fcefb0e1fa0
上村侑 <kamiyuonep@yahoo.co.jp>
2019-12-18 12:23:54 +0000

    phpセクション3課題です。よろしくお願いします。

---
+++ practice3.php
@@ -0,0 +1,31 @@
+<?php
+
+//課題phpセクション3....
+
+$name = 'Yu';
+
+if ($name = 'Yu'){
+ echo 'あなたの名前です。';
+}else {
+ echo 'あなたの名前ではありません。';
+}
+
+$total = 0;
+
+for ($i = 1; $i <= 10000; $i++) {
+  $total += $i; 
+}
+echo $total;
+
+$fruits = ['orange','apple','strawberry','cherry','banana'];
+ foreach ($fruits as $value) {
+  echo $value;
+}
+
+$i = 0;
+for ($i = 1; $i <= 100; $i++){
+  if ($i % 5 == 0) {
+    echo "\n";
+    echo $i;
+  }
+}
